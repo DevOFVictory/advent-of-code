172feb0f5ce156aadce6710daa579c7b8248ea81
DevOFVictory <devofvictory@gmail.com>
2021-12-03 00:42:58 +0100

    Added 08/12/2020

---
+++ 2020/8/script_1.py
@@ -0,0 +1,30 @@
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+    position = 0
+    accumulator = 0
+
+    l = []
+
+    while position not in l:
+        splitted = lines[position].split()
+        l.append(position)
+
+        command = splitted[0]
+        sign = splitted[1][0]
+        value = int(splitted[1][1:])
+
+        if command == 'acc':
+            if sign == '+':
+                accumulator += value
+            else:
+                accumulator -= value
+            position += 1
+        elif command == 'jmp':
+            if sign == '+':
+                position += value
+            else:
+                position -= value
+        else:
+            position += 1
+    print(accumulator)
