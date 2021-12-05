42f7cd495e761d701a4e27d2f8799ffb609a26e0
DevOFVictory <devofvictory@gmail.com>
2021-12-05 18:58:50 +0100

    Added day 12/04/2021 - Part 1

--- 2021/4/script_1.py
+++ 2021/4/script_1.py
@@ -5,12 +5,12 @@ def has_row_won(row):
     return row[0] in called_numbers and row[1] in called_numbers and row[2] in called_numbers and row[3] in called_numbers and row[4] in called_numbers
 
 
-def has__field_won(field):
-    for i in field:
-        if (has_row_won(i)):
+def has_field_won(field):
+    for row_num in field:
+        if has_row_won(list(map(int, row_num))):
             return True
-    for i in list(map(list, zip(*field))):
-        if (has_row_won(i)):
+    for collumn_num in list(map(list, zip(*field))):
+        if has_row_won(list(map(int, collumn_num))):
             return True
     return False
 
@@ -19,17 +19,30 @@ with open('input.txt') as f:
     numbers = list(map(int, f.readline().split(',')))
 
     fields = [i.strip() for i in f.read().strip().split('\n\n')]
+    fields = [i.split('\n') for i in fields]
+    fields = [[list(map(int, j.split())) for j in i] for i in fields]
 
     winner = -1
-    counter = 0
+    counter = -1
 
     while winner == -1:
-        for index, f in enumerate(fields):
-            field = [i.strip().split() for i in f.strip().split('\n')]
-            if has__field_won(field):
-                winner = index
-
-        called_numbers.append(numbers[counter])
-        print(called_numbers)
         counter += 1
-    print(winner)
+        for index, field in enumerate(fields):
+            if has_field_won(field):
+                winner = index
+                break
+            else:
+                called_numbers.append(int(numbers[counter]))
+
+    number = numbers[counter]
+    unmarked_sum = 0
+
+    for i in fields[winner]:
+        for j in i:
+            if j not in called_numbers:
+                unmarked_sum += j
+
+    print('Number:', number)
+    print('Winner: ', winner)
+    print('Umarked sum:', unmarked_sum)
+    print('Soulution: ', number*unmarked_sum)
