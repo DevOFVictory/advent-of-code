338c0bf5c1c4acb1cc98e65e1f2feb9b8a4cdb2b
DevOFVictory <devofvictory@gmail.com>
2021-12-07 00:03:57 +0100

    Added day 12/05/2021 - Part 2

---
+++ 2021/5/script_2.py
@@ -0,0 +1,49 @@
+
+def get_coords_between(xStart, yStart, xEnd, yEnd):
+    points = []
+
+    if xStart == xEnd:
+        for i in range(min(yStart, yEnd), max(yStart, yEnd)+1):
+            points.append((xStart, i))
+    elif yStart == yEnd:
+        for i in range(min(xStart, xEnd), max(xStart, xEnd)+1):
+            points.append((i, yStart))
+    else:
+        if xStart > xEnd:
+            xStart, yStart, xEnd, yEnd = xEnd, yEnd, xStart, yStart
+
+        result = []
+        slope = (yEnd - yStart) // (xEnd - xStart)
+        for i, j in zip(range(xStart, xEnd), range(yStart, yEnd, slope)):
+            points.append((i, j))
+        points.append((xEnd, yEnd))
+
+    return points
+
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+    coords = []
+    for line in lines:
+        start = line.split(' -> ')[0]
+        end = line.split(' -> ')[1]
+
+        xStart = int(start.split(',')[0])
+        yStart = int(start.split(',')[1])
+
+        xEnd = int(end.split(',')[0])
+        yEnd = int(end.split(',')[1])
+
+        for coord in get_coords_between(xStart, yStart, xEnd, yEnd):
+            coords.append(coord)
+
+    MyList = ["a", "b", "a", "c", "c", "a", "c"]
+
+    dict_of_counts = {item: coords.count(item) for item in coords}
+
+    filtered = dict((k, v) for k, v in dict_of_counts.items() if v >= 2)
+
+    print(len(filtered))
+
+    # TODO: Optimize Runtime!!!!!!!!!!!!
